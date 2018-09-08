6cdca56a06275dc2420cb3b4de09f7a417245408
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-09-07 21:59:44 -0400

    heroku

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,8 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-/// <reference types="node" />
 
-console.log(process.env);
+console.log(process.env.GITHUB);
 
 if (process.env.NODE_ENV === 'production') {
   enableProdMode();
--- src/polyfills.ts
+++ src/polyfills.ts
@@ -44,9 +44,7 @@
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
 import 'core-js/es7/reflect';
-(window as any).process = {
-  env: { DEBUG: undefined },
-};
+
 
 
 /**

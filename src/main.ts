d38b889c6a3957780fea9288d70943018b988714
Brandon Fizer <30492636+Track7Dev@users.noreply.github.com>
2018-09-07 20:02:55 -0400

    trying environment vars

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+/// <reference types="node" />
 
 if (process.env.NODE_ENV === 'production') {
   enableProdMode();
